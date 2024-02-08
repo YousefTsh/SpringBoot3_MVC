f1e19a800490a1ac0c32b2a8232de0319b1d8c5b
YousefTsh <157696730+YousefTsh@users.noreply.github.com>
2024-02-08 21:05:50 +0200

    Delete src directory

--- src/main/java/com/spring/mvc/repositorys/BatchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.mvc.repositorys;
-
-import com.spring.mvc.models.batchEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BatchRepository extends JpaRepository<batchEntity,Integer> {
-
-}
--- src/main/java/com/spring/mvc/repositorys/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.mvc.repositorys;
-
-import com.spring.mvc.models.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event,Integer> {
-}
